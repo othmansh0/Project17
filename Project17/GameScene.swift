491dafb0bc15960121591b71b97f5336d29fa2e2
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-22 11:16:47 +0300

    Implement createEnemy function

--- Project17/GameScene.swift
+++ Project17/GameScene.swift
@@ -9,6 +9,14 @@
 //-------------------------------------------------------------------------------------
 
 //Irregular shapes use per-pixel collision detection
+
+//----------------------------------------------------------------------------------
+//Timer is responsible for running code after a period of time has passed, either once or repeatedly
+//Parameters:
+//seconds to delay, object to tell when it has fired,  what method should be called, any context you want to provide, and whether the time should repeat
+//--------------------------------------------------------------------------------------
+//linearDamping and angularDamping by default have values > 0 to stimulate friction in the air
+
 import SpriteKit
 import GameplayKit
 
@@ -16,6 +24,10 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
     var starField: SKEmitterNode!
     var player: SKSpriteNode!
     var scoreLabel: SKLabelNode!
+    
+    var possibleEnimies = ["ball", "hammer", "tv"]
+    var gameTimer: Timer?
+    var isGameOver = false
     
     var score = 0 {
         didSet {
@@ -32,8 +44,9 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
        //right edge of the screen and half way up
         starField.position = CGPoint(x: 1024, y: 384)
         starField.advanceSimulationTime(10)
-        starField.zPosition = -1
         addChild(starField)
+        starField.zPosition = -1
+    
        
         
         player = SKSpriteNode(imageNamed: "player")
@@ -59,11 +72,50 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
         // sets our current game scene to be the contact delegate of the physics world
         physicsWorld.contactDelegate = self
         
+        //ScheduledTimer() timer not only creates a timer, but also starts it immediately.
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        
+        //linearDamping = 0 and angular  movement and rotation will never slow down over time perfect for frictionless environement
     }
     
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        for node in children {
+            //removing nodes whenever the node passed -300
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+            //update score
+            if !isGameOver {
+                score += 1
+            }
+        }
     }
+    
+    //MARK: - Create Enemy
+    
+    @objc func createEnemy() {
+        guard let enemy = possibleEnimies.randomElement() else { return }
+        
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        //let it collide with the player
+        sprite.physicsBody?.contactTestBitMask = 1
+        //speed when moving/giving it a push
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        //Spin it in the air,give it constant spin
+        sprite.physicsBody?.angularVelocity = 5
+        //controls how fast things slows down over time/never slow it down
+        sprite.physicsBody?.linearDamping = 0
+        //never stops spining
+        sprite.physicsBody?.angularDamping = 0
+        
+   
+    }
+    
 }
